6eb32de3c3f1e8e06da2bbfd9f66f0bee76ac2f0
procarrie <procarrie@hotmail.com>
2015-02-10 13:57:21 +0900

    delete comment

--- AgensManager/app.js
+++ AgensManager/app.js
@@ -21,7 +21,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-// mmmm
+
 // development only
 if ('development' === app.get('env')) {
   app.use(express.errorHandler());
