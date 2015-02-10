744280d0af48ec7039a457f89fb83baad63339e6
johnahkim <wjddk6671@naver.com>
2015-02-10 16:50:12 +0900

    dd

--- AgensManager/app.js
+++ AgensManager/app.js
@@ -1,35 +1,65 @@
+//-------------------- app.html 불러오는 app모듈
+//var express = require('express');
+//var fs = require('fs');
+//var app = express();
+//app.use('/public', express.static("C:/Users/Johnahkim/workspace/test/public"));
 
-/**
- * Module dependencies.
- */
+//app.get('/', function (req, res){
+//fs.readFile('/Users/Johnahkim/workspace/test/app.html', function(error, data){
+//res.writeHead(200, {'Content-Type': 'text/html'});
+//res.end(data);
+//});
+//});
+//app.listen(3000, function() {
+//console.log('Server Start .');
+//});
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
 
-var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+//-------------------- postgres란 db의 public이란 schema의 테이블과 그의 칼럼들 출력 (System Views)
+var http = require('http');
+var pg = require('pg');
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+var conString = "postgres://postgres:pro1459@localhost/procarrie";
+//var conString = "postgres://postgres:1111@localhost/postgres";
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+var server = http.createServer(function(req, res) {
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+	// get a pg client from the connection pool
+	pg.connect(conString, function(err, client, done) {
+
+		var handleError = function(err) {
+			// no error occurred, continue with the request
+			if(!err) return false;
+
+			done(client);
+			res.writeHead(500, {'content-type': 'text/plain'});
+			res.end('An error occurred');
+			return true;
+		};
+
+		client.query('select * from pg_tables where schemaname=\'public\'', function(err, result) {
+			// handle an error from the query
+			if(handleError(err)) {
+				console.log(err); 
+				return;
+			}
+
+			// return the client to the connection pool for other requests to reuse
+			for(var i = 0; i < result.rows.length ; i++){
+				//console.log("tablename("+(i+1)+") : "+result.rows[i].tablename);
+				client.query('select * from information_schema.columns where table_schema=\'public\' and table_name=\''+result.rows[i].tablename+'\'', function(err, rs){
+					console.log(rs.rows[0].table_name+"'s columns are........");
+					for(var m = 0; m <rs.rows.length ; m++){
+						console.log("- "+rs.rows[m].column_name);
+					}
+					done();
+					res.writeHead(200, {'content-type': 'text/plain'});
+					res.end(rs.rows[0].table_name);
+				});
+			}
+		});
+	});
 });
+
+server.listen(3001);
