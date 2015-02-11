e4cb8d2b6fef3406c45905c965a6d3e4acf30435
procarrie <procarrie@hotmail.com>
2015-02-11 16:44:06 +0900

    * DB/Schema/Table/Column명 가져와서 배열로 저장하는 부분 추가

--- AgensManager/app.js
+++ AgensManager/app.js
@@ -37,29 +37,42 @@ var server = http.createServer(function(req, res) {
 			res.end('An error occurred');
 			return true;
 		};
-
-		client.query('select * from pg_tables where schemaname=\'public\'', function(err, result) {
-			// handle an error from the query
-			if(handleError(err)) {
-				console.log(err); 
-				return;
+		
+		// get database list
+		var arrDB = [];
+		client.query('select datname from pg_database WHERE datistemplate=\'f\';', function(err, result){
+			for(var i=0; i < result.rows.length; i++){
+				arrDB.push(result.rows[i].datname);
 			}
-
-			// return the client to the connection pool for other requests to reuse
-			for(var i = 0; i < result.rows.length ; i++){
-				//console.log("tablename("+(i+1)+") : "+result.rows[i].tablename);
-				client.query('select * from information_schema.columns where table_schema=\'public\' and table_name=\''+result.rows[i].tablename+'\'', function(err, rs){
-					console.log(rs.rows[0].table_name+"'s columns are........");
-					for(var m = 0; m <rs.rows.length ; m++){
-						console.log("- "+rs.rows[m].column_name);
-					}
-					done();
-					res.writeHead(200, {'content-type': 'text/plain'});
-					res.end(rs.rows[0].table_name);
-				});
+			console.log(arrDB);
+		});
+		
+		// get schema list
+		var arrSch = [];
+		client.query('SELECT DISTINCT(table_schema) FROM information_schema.tables WHERE table_type = \'BASE TABLE\' AND table_schema NOT IN (\'pg_catalog\', \'information_schema\');', function(err, rs){
+			for(var j=0; j < rs.rows.length; j++){
+				arrSch.push(rs.rows[j].table_schema);
 			}
+			console.log(arrSch);
+		});
+		
+		//get table list
+		var arrTab = [];
+		client.query('SELECT DISTINCT(table_name) FROM information_schema.tables WHERE table_schema = \'testschema\' ORDER BY table_name ASC;', function(err, trs){
+			for(var k=0; k < trs.rows.length; k++){
+				arrTab.push(trs.rows[k].table_name);
+			}
+			console.log(arrTab);
+		});
+		
+		//get column list
+		var arrCol = [];
+		client.query('SELECT column_name FROM information_schema.columns WHERE table_name = \'tableschema\';', function(err, crs){
+			for(var l=0; l < crs.rows.length; l++){
+				arrCol.push(crs.rows[l].column_name);
+			}
+			console.log(arrCol);
 		});
 	});
 });
-
 server.listen(3001);
