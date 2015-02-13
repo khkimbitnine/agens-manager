8720cf028bf1860ce3c0dc502926e91de925a447
johnahkim <wjddk6671@naver.com>
2015-02-13 10:27:44 +0900

    변경

--- AgensManager/app.js
+++ AgensManager/app.js
@@ -1,33 +1,27 @@
 //-------------------- app.html 불러오는 app모듈
-//var express = require('express');
-//var fs = require('fs');
-//var app = express();
-//app.use('/public', express.static("C:/Users/Johnahkim/workspace/test/public"));
-
-//app.get('/', function (req, res){
-//fs.readFile('/Users/Johnahkim/workspace/test/app.html', function(error, data){
-//res.writeHead(200, {'Content-Type': 'text/html'});
-//res.end(data);
-//});
-//});
-//app.listen(3000, function() {
-//console.log('Server Start .');
-//});
-
-
-
-//-------------------- postgres란 db의 public이란 schema의 테이블과 그의 칼럼들 출력 (System Views)
+var express = require('express');
+var fs = require('fs');
+var app = express();
 var http = require('http');
 var pg = require('pg');
+//========constring
+var conString = "postgres://postgres:1111@localhost/agens";
+//======== db, schema, table, column 배열 선언
+var arrDB = [];
+var arrSch = [];
+var arrTab = [];
+var arrCol = [];
+//======== 클라이언트에서 받은 db, schema, table, column 이름 담을 변수 선언
+var sc = null;
+app.use('/public', express.static("C:/Users/Johnahkim/workspace/test/public"));
 
-var conString = "postgres://postgres:pro1459@localhost/procarrie";
-//var conString = "postgres://postgres:1111@localhost/postgres";
-
-var server = http.createServer(function(req, res) {
-
-	// get a pg client from the connection pool
+app.set('port', 3000);
+var server = http.createServer(app).listen(app.get('port'), function(){
+	console.log('server listening on 3000');
+	
+	
 	pg.connect(conString, function(err, client, done) {
-
+		
 		var handleError = function(err) {
 			// no error occurred, continue with the request
 			if(!err) return false;
@@ -39,7 +33,7 @@ var server = http.createServer(function(req, res) {
 		};
 		
 		// get database list
-		var arrDB = [];
+		
 		client.query('select datname from pg_database WHERE datistemplate=\'f\';', function(err, result){
 			for(var i=0; i < result.rows.length; i++){
 				arrDB.push(result.rows[i].datname);
@@ -48,25 +42,26 @@ var server = http.createServer(function(req, res) {
 		});
 		
 		// get schema list
-		var arrSch = [];
-		client.query('SELECT DISTINCT(table_schema) FROM information_schema.tables WHERE table_type = \'BASE TABLE\' AND table_schema NOT IN (\'pg_catalog\', \'information_schema\');', function(err, rs){
+		
+		client.query('select schema_name from information_schema.schemata where schema_name not like \'pg_%\' and schema_name <> \'information_schema\'', function(err, rs){
 			for(var j=0; j < rs.rows.length; j++){
-				arrSch.push(rs.rows[j].table_schema);
+				arrSch.push(rs.rows[j].schema_name);
 			}
 			console.log(arrSch);
 		});
 		
 		//get table list
-		var arrTab = [];
-		client.query('SELECT DISTINCT(table_name) FROM information_schema.tables WHERE table_schema = \'testschema\' ORDER BY table_name ASC;', function(err, trs){
-			for(var k=0; k < trs.rows.length; k++){
-				arrTab.push(trs.rows[k].table_name);
-			}
-			console.log(arrTab);
-		});
+		if(sc!=null){
+			client.query('SELECT DISTINCT(table_name) FROM information_schema.tables WHERE table_schema = \''+sc+'\' ORDER BY table_name ASC;', function(err, trs){
+				for(var k=0; k < trs.rows.length; k++){
+					arrTab.push(trs.rows[k].table_name);
+				}
+				console.log(arrTab);
+			});
+		}
 		
 		//get column list
-		var arrCol = [];
+		
 		client.query('SELECT column_name FROM information_schema.columns WHERE table_name = \'tableschema\';', function(err, crs){
 			for(var l=0; l < crs.rows.length; l++){
 				arrCol.push(crs.rows[l].column_name);
@@ -74,5 +69,38 @@ var server = http.createServer(function(req, res) {
 			console.log(arrCol);
 		});
 	});
+	
+	
+	
 });
-server.listen(3001);
+//========= app.html읽기
+app.get('/', function (req, res){
+	fs.readFile('/Users/Johnahkim/workspace/test/app.html', function(error, data){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(data);			//ja021017
+	});
+});
+//========= socket.io 선언
+var io = require('socket.io').listen(server);
+
+//========= socket 연결
+io.on('connection', function(socket){
+		//=========== 접속한 db 리스트 전송
+		socket.emit('db', {db: arrDB});
+		//=========== db 클릭하면, 스키마 전송
+		socket.on('set_dbname', function (dbname){
+			if(dbname!=null){
+				socket.emit('scname', {schema: arrSch});
+			}
+		});
+		//=========== schema 클릭하면, 테이블 전송
+		socket.on('set_scname', function (scname){
+			if(scname!=null){
+				sc = scname;
+				console.log(sc);
+				console.log(arrTab);
+				socket.emit('tabname', {table: arrTab});
+			}
+		});
+});
+
