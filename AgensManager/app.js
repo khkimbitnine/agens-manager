e37f4e17fb016e42716dedd7ad7a746d29a18b7e
johnahkim <wjddk6671@naver.com>
2015-02-24 10:51:00 +0900

    - view에 하위항목으로 컬럼 추가

--- AgensManager/app.js
+++ AgensManager/app.js
@@ -179,8 +179,6 @@ var server = http.createServer(app).listen(app.get('port'), function(){
 			});
 			//스키마 함수 클릭, 함수 전송
 			socket.on('set_scname_func', function (scname){
-//				for(var i = 0 ; i < arrSch.length ; i++){
-//					if(arrSch[i] === scname){
 						arrFunc = [];
 						//get function list
 						client.query('select proname from pg_proc inner join pg_namespace ns on (pg_proc.pronamespace = ns.oid) where ns.nspname = \''+scname+'\' order by proname;', function(err, frs){
@@ -188,15 +186,11 @@ var server = http.createServer(app).listen(app.get('port'), function(){
 							socket.emit('funcname', {func: arrFunc});
 							console.log(arrFunc);
 						});
-//					}
-//				}
 			});
 			
 
 			//테이블 컬럼 클릭, 컬럼 전송
 			socket.on('set_tabname_col', function (data){
-//				for(var i = 0 ; i < arrTab.length ; i++){
-//					if(arrTab[i] === data.tabname){
 						arrCol = [];
 						//get column list
 						client.query('SELECT column_name FROM information_schema.columns WHERE table_schema=\''+data.scname+'\' and table_name =\''+data.tabname+'\' order by column_name;', function(err, crs){
@@ -204,14 +198,10 @@ var server = http.createServer(app).listen(app.get('port'), function(){
 							socket.emit('colname', {column: arrCol});
 							console.log(arrCol);
 						});
-//					}
-//				}
 			});
 			
 			//테이블 제약키 클릭, 제약키 전송
 			socket.on('set_tabname_cons', function (data){
-//				for(var i = 0 ; i < arrTab.length ; i++){
-//					if(arrTab[i] === data.tabname){
 						arrCons = [];
 						//get constraint list
 						client.query('select constraint_name from information_schema.constraint_column_usage where table_schema=\''+data.scname+'\' and table_name=\''+data.tabname+'\' order by constraint_name;', function(err, consrs){
@@ -219,23 +209,17 @@ var server = http.createServer(app).listen(app.get('port'), function(){
 							socket.emit('consname', {constraint: arrCons});
 							console.log(arrCons);
 						});
-//					}
-//				}
 			});
 			
 			//테이블 인덱스 클릭, 인덱스 전송
 			socket.on('set_tabname_ind', function (data){
-//				for(var i = 0 ; i < arrTab.length ; i++){
-//					if(arrTab[i] === data.tabname){
 						arrInd = [];
 						//get index list
-						client.query('select relname from pg_class c, pg_index i, (select distinct(relfilenode) from pg_index, pg_class, (select oid from pg_namespace where nspname = \''+data.scname+'\') s where relname = \''+data.tabname+'\' and relnamespace=s.oid) t where c.relkind = \'i\' AND c.relname NOT LIKE \'pg%\' and i.indexrelid = c.oid and i.indrelid = t.relfilenode and c.oid NOT IN (select conindid from pg_constraint);', function(err, irs){
+						client.query('select relname from pg_class c, pg_index i, (select distinct(relfilenode) from pg_class, (select oid from pg_namespace where nspname = \''+data.scname+'\') s where relname = \''+data.tabname+'\' and relnamespace=s.oid) t where c.relkind = \'i\' and i.indexrelid = c.oid and i.indrelid = t.relfilenode and c.oid NOT IN (select conindid from pg_constraint);', function(err, irs){
 							selectInd(err, irs);
 							socket.emit('indname', {index: arrInd});
 							console.log(arrInd);
 						});
-//					}
-//				}
 			});
 			
 		});
