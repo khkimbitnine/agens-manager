1bc928509ffe676ae3706c88396f39d5d9777599
jakim <jakim@bitnine.co.kr>
2015-04-09 21:20:42 +0900

    - type 스키마조건으로 dataType가져오기 - setof, 배열을 체크박스화 - phppgAdmin - trigger function 대로 쿼리 가져옴

--- AgensManager/create_trigger.js
+++ AgensManager/create_trigger.js
@@ -1,7 +1,7 @@
 exports.create_trigger = function(socket, client){
 socket.on('username', function(username){
 	var proname = [];
-	client.query("select proname from pg_proc, (select usesysid from pg_user where usename='"+username+"') u where proowner=u.usesysid", function(err, rs){
+	client.query("select proname from pg_proc, (select usesysid from pg_user where usename='"+username+"') u where proowner=u.usesysid or prorettype = 2279 order by proname", function(err, rs){
 		if(err){
 			console.log(err);
 		}else{
@@ -9,7 +9,6 @@ socket.on('username', function(username){
 				proname.push(rs.rows[i].proname);
 			}
 			socket.emit('trigger_function', proname);
-			console.log(proname);
 		}
 	});
 });
